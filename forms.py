9016ed71a9523ce75cbe85686f77fb35c4cf3189
Sue Holder <susanpholder@gmail.com>
2020-04-28 21:19:51 +0100

    added form to register.html and passed in the form instance from form.py

--- forms.py
+++ forms.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('username', validators=[
+    username = StringField('Username', validators=[
                            DataRequired(), Length(min=2, max=20)])
     email = StringField('Email Address', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
