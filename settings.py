678210d91c6765ea4a5445657d29cfc4660e44aa
Sue Holder <susanpholder@gmail.com>
2020-05-02 11:37:56 +0100

    fixed error on PORT settings

--- settings.py
+++ settings.py
@@ -3,12 +3,11 @@ import os
 MONGO_URI = os.environ.get("CODEFLOW_MONGO_URI")
 MONGO_DBNAME = os.environ.get("")
 SECRET_KEY = os.environ.get("CODEFLOW_SECRET_KEY")
-HOST= os.environ.get('IP')
-PORT =  int(os.environ.get('PORT'))
+HOST = os.environ.get('IP')
+PORT = os.environ.get('PORT')
 
 
 RECAPTCHA_PUBLIC_KEY = os.environ.get("CODEFLOW_RECAPTCHA_PUBLIC_KEY")
 RECAPTCHA_PRIVATE_KEY = os.environ.get("CODEFLOW_RECAPTCHA_PRIVATE_KEY")
 RECAPTCHA_USE_SSL = False
 RECAPTCHA_OPTIONS = {'theme': 'white'}
-
