1ed34355fb3648d1800bb0128987f0029628fc01
Sue Holder <susanpholder@gmail.com>
2020-05-02 15:37:06 +0100

    trying to resolve heroku errors

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 MONGO_URI = os.environ.get("CODEFLOW_MONGO_URI")
 MONGO_DBNAME = os.environ.get("")
 SECRET_KEY = os.environ.get("CODEFLOW_SECRET_KEY")
-HOST = os.environ.get('IP')
+IP = os.environ.get('IP')
 PORT = os.environ.get('PORT')
 
 
