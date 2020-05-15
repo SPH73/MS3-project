bd23e00e09f3a2fd00593da4c87a837d88a3c42b
Sue Holder <susanpholder@gmail.com>
2020-05-15 16:54:01 +0100

    updated blog.html with better accordion, styling and js

---
+++ app/static/js/scripts.js
@@ -0,0 +1,18 @@
+// dynamic accordion
+
+let coll = document.getElementsByClassName("accordion-label");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].onclick = function () {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        console.log(content);
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
+}
