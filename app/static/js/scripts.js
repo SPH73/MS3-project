29effd2652fb277d24f70b06ab251db7c223801b
Sue Holder <susanpholder@gmail.com>
2020-05-29 16:22:41 +0100

    added missing comment

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -17,11 +17,8 @@ for (i = 0; i < coll.length; i++) {
     }
 }
 
-
-$(document).ready(function () {
-    // dynamic input field for project form pieces
-    $("#add-piece").click(function () {
-        let addPiece = '<input class="form-control form-control-lg" id="pieces-0-piece" name="pieces-0-piece" type="text" value="">';
-        $('input').append(addPiece);
-    })
+// dynamic input field for project form pieces
+$("#add-piece").click(function () {
+    let addPiece = '<input class="form-control form-control-lg" id="pieces-0-piece" name="pieces-0-piece" type="text" value="">';
+    $('input').append(addPiece);
 })
