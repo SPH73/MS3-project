b8b0c8eb74641e85cc4ce969b2c3161bbfadd4ce
Sue Holder <susanpholder@gmail.com>
2020-06-15 23:06:58 +0100

    added footer chnaged site styling, formatted docstrings to convention doublequotes

--- app/routes.py
+++ app/routes.py
@@ -18,13 +18,13 @@ def home():
 
 # USER ACCOUNT VIEWS/ACTIONS
 
-@app.route("/register", methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    '''Checks if user has an active session, if not, checks if the email address exists, if not and username isn't taken then creates user.
-    '''
+    """Checks if user has an active session, if not, checks if the email address exists, if not and username isn't taken then creates user.
+    """
     
     if 'username' in session:    
-        flash("You are already registered and logged in. Did you mean to go to your dashboard instead?", 'info')
+        flash('You are already registered and logged in. Did you mean to go to your dashboard instead?', 'info')
         return redirect(url_for('dashboard'))
         
     form = RegistrationForm()
@@ -56,13 +56,13 @@ def register():
         
     return render_template('pages/register.html', title='Register', form=form)
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    '''First checks if user has an active session, if not, checks the users credentials are correct, if so logs them in and adds the username to the session.
-    '''
+    """First checks if user has an active session, if not, checks the users credentials are correct, if so logs them in and adds the username to the session.
+    """
     
     if 'username' in session:
-        flash("You are already logged in. Did you mean to go to your dashboard instead?", 'info')
+        flash('You are already logged in. Did you mean to go to your dashboard instead?', 'info')
         return redirect(url_for('dashboard'))
         
     form = LoginForm()
@@ -78,13 +78,13 @@ def login():
        
     return render_template('pages/login.html', title='Login', form=form)
 
-@app.route("/forgot_password", methods=['GET', 'POST'])
+@app.route('/forgot_password', methods=['GET', 'POST'])
 def forgot_password():
-    '''First checks if user has an active session, if not, checks if the email address exists and then redirects to reset password.
-    '''
+    """First checks if user has an active session, if not, checks if the email address exists and then redirects to reset password.
+    """
     
     if 'username' in session:    
-        flash("You are already logged in, you can reset your password here.", 'info')
+        flash('You are already logged in, you can reset your password here.', 'info')
         return redirect(url_for('dashboard'))
         
     form = ForgotPasswordForm()
@@ -94,19 +94,18 @@ def forgot_password():
             user = mongo.db.user.find_one({'email':form.email.data})
 
             if user:
-                # Consider adding a security passphrase instead/as well
-                flash("Please enter your security passphrase and create a new password", 'info')
+                flash('Please enter your security passphrase and create a new password', 'info')
                 return redirect(url_for('reset_password'))                        
                 
-            flash("Email address not found!", 'danger')
+            flash('Email address not found!', 'danger')
             return render_template('pages/forgot.html', title='Forgot Password', form=form)
                 
     return render_template('pages/forgot.html', title='Forgot Password', form=form)
 
-@app.route("/reset_password", methods=['GET', 'POST'])
+@app.route('/reset_password', methods=['GET', 'POST'])
 def reset_password():
-    '''First checks if passphrase and username are correct then updates new password in database.
-    '''   
+    """First checks if passphrase and username are correct then updates new password in database.
+    """   
           
     form = ResetPasswordForm()
     if request.method == 'POST':
@@ -124,10 +123,10 @@ def reset_password():
             
     return render_template('pages/reset.html', title='Forgot Password', form=form)
 
-@app.route("/update_password", methods=['GET', 'POST'])
+@app.route('/update_password', methods=['GET', 'POST'])
 def update_password():
-    '''First checks if the users current password is correct then updates the password in the database.
-    '''   
+    """First checks if the users current password is correct then updates the password in the database.
+    """   
     
     form = PasswordForm()
     if request.method == 'POST':
@@ -147,18 +146,18 @@ def update_password():
 
 @app.route('/logout')
 def logout():
-    '''Removes the username from the session.
-    '''
+    """Removes the username from the session.
+    """
     
     session.pop('username', None)
     flash('You have logged out.', 'success')
     return redirect(url_for('home'))
     
 
-@app.route("/dashboard", methods=['GET', 'POST'])
+@app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    '''First checks the user is logged in then renders the users content to the dashboard template.
-    '''
+    """First checks the user is logged in then renders the users content to the dashboard template.
+    """
     
     if 'username' in session:
         user_profile = mongo.db.profiles.find_one({'username': session['username']})
@@ -177,20 +176,20 @@ def dashboard():
 
 # BLOG ARTICLE VIEWS
 
-@app.route("/blog")
+@app.route('/blog')
 def blog():
-    '''Retrieves all the documents from the articles collection and dislpay them in order of newest to oldest.
-    '''
+    """Retrieves all the documents from the articles collection and dislpay them in order of newest to oldest.
+    """
     
     articles = mongo.db.articles.find().sort('date',pymongo.DESCENDING)
                      
-    return render_template('pages/blog.html', title='Blog', articles=articles)
+    return render_template('pages/blog.html', title='Blog', articles=articles, legend='Read the latest articles')
 
 
-@app.route("/add_article", methods=['GET','POST'])
+@app.route('/add_article', methods=['GET','POST'])
 def add_article():
-    '''Checks if the user is logged in then renders the form to add a blog article. If the form validates, retrieves the users id and includes it with the form data to the articles collection.
-    '''
+    """Checks if the user is logged in then renders the form to add a blog article. If the form validates, retrieves the users id and includes it with the form data to the articles collection.
+    """
     
     if 'username' in session:           
         form=BlogForm()        
@@ -211,17 +210,15 @@ def add_article():
     flash('You need to be logged in to post any content.', 'info')
     return redirect(url_for('login'))
 
-# TODO
-# - EDIT AND DELETE 
 
 @app.route('/edit_article/<article_id>')
 def edit_article(article_id):
-    '''First checks that the user is logged in then renders the form with the data from the collection document. (Edit button only appears if there is a match with the user id in the database for the particular document id).
-    '''
+    """First checks that the user is logged in then renders the form with the data from the collection document. (Edit button only appears if there is a match with the user id in the database for the particular document id).
+    """
     
     if 'username' in session: 
         article = mongo.db.articles.find_one_or_404(
-            {"_id": ObjectId(article_id)})
+            {'_id': ObjectId(article_id)})
         form=BlogForm()
         form.title.data = article['title']
         form.content.data = article['content']
@@ -229,14 +226,15 @@ def edit_article(article_id):
 
 @app.route('/update_article/<article_id>', methods=['POST'])
 def update_article(article_id):
-    '''When the user submits the edited content, the document in the collection is updated with the changes and redirects the user to the blog page.
-    '''
+    """When the user submits the edited content, the document in the collection is updated with the changes and redirects the user to the blog page.
+    """
     
     article = mongo.db.articles
     article.find_one_and_update({'_id': ObjectId(article_id) },
                                 {'$set':
                                     {'title': request.form.get('title'),
                                      'content': request.form.get('content')}})
+    flash('Your post has been updated.' 'success')
     return redirect(url_for('blog'))
     
 
@@ -252,8 +250,8 @@ def delete_article(article_id):
 
 @app.route('/add_comment/<article_id>', methods=['GET', 'POST'])
 def add_comment(article_id):
-    '''If a user is logged in, adds a comment in the article collection comments array and creates a reference document in the comments collection for user dashboards.
-    '''
+    """If a user is logged in, adds a comment in the article collection comments array and creates a reference document in the comments collection for user dashboards.
+    """
 
     article = mongo.db.articles
     if 'username' in session:
@@ -282,8 +280,8 @@ def add_comment(article_id):
 
 @app.route("/projects")
 def projects():
-    '''First check that the user is logged in, then display all the projects in the database sorted by newest first.
-    '''
+    """First check that the user is logged in, then display all the projects in the database sorted by newest first.
+    """
     
     if 'username' in session:
         current_user = mongo.db.user.find_one({'username': session['username']})      
@@ -294,10 +292,10 @@ def projects():
     return redirect(url_for('login'))
 
                  
-@app.route("/add_project", methods=['GET','POST'])
+@app.route('/add_project', methods=['GET','POST'])
 def add_project():
-    '''First checks that the user is logged in before rendering the form. When the form is validated, the users user_id is retireved and add with the form content with the current date and time to the collection.
-    '''
+    """First checks that the user is logged in before rendering the form. When the form is validated, the users user_id is retireved and add with the form content with the current date and time to the collection.
+    """
     
     if 'username' in session:        
         form=ProjectForm()
@@ -324,12 +322,12 @@ def add_project():
 
 @app.route('/edit_project<project_id>')
 def edit_project(project_id):
-    '''First checks that the user is logged in then renders the form with the data from the collection document. (Edit button only appears if there is a match with the user id in the database for the particular document id and the users session data).
-    '''
+    """First checks that the user is logged in then renders the form with the data from the collection document. (Edit button only appears if there is a match with the user id in the database for the particular document id and the users session data).
+    """
     
     if 'username' in session: 
         project = mongo.db.projects.find_one_or_404(
-            {"_id": ObjectId(project_id)})
+            {'_id': ObjectId(project_id)})
         form=ProjectForm()
         form.title.data = project['title']
         form.status.data = project['status']
@@ -339,22 +337,22 @@ def edit_project(project_id):
 
 @app.route('/update_project<project_id>', methods=['POST'])
 def update_project(project_id):
-    '''When the user submits the edited content, the document in the collection is updated with the changes and redirects the user to the projects page.
-    '''
+    """When the user submits the edited content, the document in the collection is updated with the changes and redirects the user to the projects page.
+    """
 
     project = mongo.db.projects
     project.find_one_and_update({'_id': ObjectId(project_id) },
                                 {'$set':
                                     {'title': request.form.get('title'),
                                      'status': request.form.get('status'),
-                                     'deadline': datetime.strptime(request.form.get('deadline'), "%m/%d/%Y"),
+                                     'deadline': datetime.strptime(request.form.get('deadline'), '%m/%d/%Y'),
                                      'brief': request.form.get('brief')}})
     return redirect(url_for('projects'))
 
 @app.route('/delete_project<project_id>', methods=['POST'])
 def delete_project(project_id):
-    '''If the user is logged in and the user id is located in the document then the document is removed from the collection. (Client-side confirmation is passed before the user reaches this view function.)
-    '''
+    """If the user is logged in and the user id is located in the document then the document is removed from the collection. (Client-side confirmation is passed before the user reaches this view function.)
+    """
     
     project = mongo.db.projects
     project.delete_one({'_id': ObjectId(project_id)})
@@ -363,6 +361,9 @@ def delete_project(project_id):
 
 @app.route('/add_piece/<title>', methods=['GET', 'POST'])
 def add_piece(title):
+    """TO DO WHEN function complete!!!!
+    """
+    
     if 'username' in session:
         form=PieceForm()
         
@@ -388,8 +389,8 @@ def add_piece(title):
 
 @app.route("/profiles")
 def profiles():
-    '''Checked if is the user is logged in first and then render page.
-    '''
+    """Checks if is the user is logged in first and then renders page with profile collection data.
+    """
     
     if 'username' in session:
         profiles = mongo.db.profiles.find()
@@ -400,8 +401,8 @@ def profiles():
 
 @app.route('/add_profile', methods=['GET','POST'])
 def add_profile():
-    '''Checks if the user has an active session and an exisiting profile for the user doesn't exist before renders the form. When the form is validated first retrieves the user_id to include it with the form data to the database collection with the current date and time.
-    '''
+    """Checks if the user has an active session and an exisiting profile for the user doesn't exist before renders the form. When the form is validated first retrieves the user_id to include it with the form data to the database collection with the current date and time.
+    """
     
     if 'username' in session:
         user = mongo.db.user.find_one({'username': session['username']})           
@@ -434,11 +435,11 @@ def add_profile():
     
 @app.route('/edit_profile/<profile_id>')
 def edit_profile(profile_id):
-    '''If the user is logged in and the user_id matches with the document, the form is rendered with the existing values. 
-    '''
+    """Checks if the user is logged in and the user_id matches with the document, then renders the form with the existing values. 
+    """
     
     profile = mongo.db.profiles.find_one_or_404(
-        {"_id": ObjectId(profile_id)})
+        {'_id': ObjectId(profile_id)})
     form=ProfileForm()
     form.headline.data = profile['headline']
     form.bio.data = profile['bio']
@@ -448,8 +449,8 @@ def edit_profile(profile_id):
     
 @app.route('/update_profile/<profile_id>', methods=['POST'])
 def update_profile(profile_id):
-    '''When the form is submitted, the collection is updated with the changes and the current date and time.
-    '''
+    """When the form is submitted, the collection is updated with the changes and the current date and time.
+    """
     
     profile = mongo.db.profiles
     profile.find_one_and_update({'_id': ObjectId(profile_id)},
@@ -463,8 +464,8 @@ def update_profile(profile_id):
 
 @app.route('/delete_profile/<profile_id>', methods=['POST'])
 def delete_profile(profile_id):
-    '''If the user is logged in and their id matches the document, the document is removed from the database collection. (Client-side validation passed before the user reaches the view function.)
-    '''
+    """If the user is logged in and their id matches the document, the document is removed from the database collection. (Client-side validation passed before the user reaches the view function.)
+    """
     
     profile = mongo.db.profiles
     profile.delete_one({'_id': ObjectId(profile_id)})
