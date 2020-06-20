711650ba23216e890abff3390d68c38a5adee18b
Sue Holder <susanpholder@gmail.com>
2020-06-20 18:52:31 +0100

    removed bs tabs not working added custon tabs and styled to match main site nav

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -28,11 +28,11 @@ $('#deadline').datepicker({
 });
 
 // toggle blog comment form
-
 $(".comment-btn").click(function (event) {
-    let id = event.target.id;
+
+    // let id = event.target.id;
     let btn = this;
-   
+
     if (btn.innerText == "Close") {
         btn.innerText = "Comment"
     } else {
@@ -43,3 +43,24 @@ $(".comment-btn").click(function (event) {
     $(form).toggleClass('hide');
 
 })
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    let i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
