5fe32971a9f252522eb45d1892a554fa32e66ce7
Sue Holder <susanpholder@gmail.com>
2020-06-29 18:07:32 +0100

    added max image size and imported into routes.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ app.config.from_object(Config)
 images = os.environ.get('IMAGES_UPLOADS')
 files = os.environ.get('FILE_UPLOADS')
 ALLOWED_IMAGE_EXTENSIONS = ['png', 'jpg', 'jpeg']
+MAX_IMAGE_SIZE = 5 * 1024 ** 2
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 mongo = PyMongo(app)
