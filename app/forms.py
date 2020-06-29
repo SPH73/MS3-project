60fac270d9bc767d5bcdd61e5bee7a8fea43d501
Sue Holder <susanpholder@gmail.com>
2020-06-29 19:07:29 +0100

    added notes/comments to project and piece forms and updated dashboard submitted pieces status

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from flask_ckeditor import CKEditorField
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, FileField, IntegerField, SelectField, FieldList, FormField, DateField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, FileField, IntegerField, SelectField, FieldList, FormField, DateField, Form
 from wtforms.validators import DataRequired, Email, EqualTo, Length, NumberRange, ValidationError
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from werkzeug.utils import secure_filename
 from datetime import datetime 
 
 
@@ -60,33 +59,39 @@ class BlogForm(FlaskForm):
 class PieceForm(FlaskForm):
     task = StringField('Task name *',validators=[DataRequired()])
     description = TextAreaField('Describe the task and expectations *', validators=[DataRequired()])
-    status = StringField('Status *', default='pending acceptance', validators=[DataRequired()])
+    status = SelectField('Status *', choices=['pending acceptance', 'accepted','submitted', 'closed'], validators=[DataRequired()])
     username = StringField('Username for whom the piece has been created *', validators=[DataRequired()])
     due_date = StringField('Due date *', validators=[DataRequired()])
-    comment = StringField('Add a comment for your reference (not required)')
+    comment = StringField('Add a comment for your reference (recommended)')
+    file_uploads = FileField('Upload a file (.pdf, .txt ,  .rtf )',validators=[FileAllowed(['pdf', 'txt', 'rtf'])])
     add_piece = SubmitField('Send Piece')
   
     
 class ProjectForm(FlaskForm):
     title = StringField('Title *', validators=[DataRequired()])
-    status = StringField('Status *', default='open' )
+    status = SelectField('Status *', choices=[('open', 'closed')], validators=[DataRequired()] )
     deadline = StringField('Set deadline', validators=[DataRequired()])
     brief = CKEditorField('Project description *', validators=[DataRequired()])
+    note = StringField('Add a personal note (only visible on your dashboard)', validators=[Length(max=250, message='Must be a maximum of %d characters long (approximately 40 words).' % (250))])
     submit = SubmitField('Post Project')
        
-             
-class ListForm(FlaskForm):  
-    skill = StringField('Skills *', default="Frontend Development")
-    language = StringField('Languages *', default="CSS3")
-    framework = StringField('Frameworks *', default="Bootstrap4")
-    link = StringField('Personal Links *', default="https/github.com/username") 
+class LanguageForm(Form):
+    language = StringField('Add language', validators=[DataRequired()])
+
+class FramworkForm(Form):
+    frameworks = StringField('Add framework', validators=[DataRequired()])
+
+class LinkForm(Form):
+    name = StringField('Link name', description='e.g. GitHub')
+    url = StringField('Link URL',description='e.g. https://github.com/yourusername')
     
 class ProfileForm(FlaskForm):
     headline = StringField('Headline *', validators=[DataRequired(), Length(min=30, max=150)])
     bio = CKEditorField('Bio: *', validators=[Length(min=30, max=1000)])
     xp = IntegerField('Years experience *', validators=[NumberRange(min=0)])
     interests = TextAreaField("What type of projects are you interested in?", validators=[DataRequired()])
-    # languages = FieldList(FormField(ListForm), min_entries=1)
-    # frameworks = FieldList(FormField(ListForm), min_entries=1)
-    # links = FieldList(FormField(ListForm), min_entries=1)
+    skills = SelectField('Select', choices=['Frontend', 'Backend', 'FullStack'])
+    languages = FieldList(FormField(LanguageForm), min_entries=1)
+    frameworks = FieldList(FormField(FramworkForm), min_entries=1)
+    links = FieldList(FormField(LinkForm), min_entries=1)
     submit = SubmitField('Send')
